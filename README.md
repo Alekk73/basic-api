ba7e88bb058ac50f5d8375e1eee311bb54e3e64e
Alekk73 <sntxdev@gmail.com>
2026-02-15 17:36:40 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### API Basica
+
+Repositorio de pruebas
---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+let port: number;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new ConfigService();
+  app.setGlobalPrefix('api');
+
+  port = Number(config.get<number>('PORT'));
+  await app.listen(port);
+}
+bootstrap()
+  .then(() => {
+    console.log(
+      `Application started successfully on http://localhost:${port}/api`,
+    );
+  })
+  .catch((error) => {
+    console.error('Failed application:', error);
+  });
